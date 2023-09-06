362bbea0c7977199f7297be7f452c3e4fbb6d046
erolemir <emirhaanerol@gmail.com>
2023-09-06 23:10:48 +0300

    Admin İşlemleri

--- project1/product/admin.py
+++ project1/product/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category,Product
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["title", "status"]
+    list_filter = ["status"]
+    
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title","category","price", "status"]
+    list_filter = ["category","status"]
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Product,ProductAdmin)
--- project1/product/models.py
+++ project1/product/models.py
@@ -12,7 +12,28 @@ class Category(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)    
     slug = models.SlugField()
-    parent = models.ForeignKey('self', blank=True, related_name='children', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.title
+    
+class Product(models.Model):
+    STATUS= (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    category = models.ForeignKey(Category,on_delete=models.CASCADE) # Category tablosuyla ilişki kurulması
+    title = models.CharField(max_length=30)
+    keyword = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    price = models.FloatField()
+    amount = models.IntegerField()
+    detail = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)    
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     
